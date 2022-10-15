aa5a97b6c955951826a061c507591d76d31f4887
Eric R <ryttingeric@gmail.com>
2022-10-14 19:58:52 -0600

    added formatting and ability to use other array generation methods. added size limit to prevent out of memory errors.

--- src/A04.java
+++ src/A04.java
@@ -6,71 +6,84 @@ import java.util.Arrays;
 
 public class A04 {
     static final double BILLION = 1_000_000_000;
+    private static String[] genMode = {
+            "random",
+            "mostly sorted",
+            "reverse order",
+            "sorted"
+    };
 
     private static void getKnownSortTestResults(){
-        //TIME TESTS:
-        //System.out.println("= = = = TESTING " +  + " SORT = = = =");
-        //System.out.println("\nTIME:");//TODO need different header
-        int size = 1;
-        boolean insertionTimedOut = false;
-        boolean selectionTimedOut = false;
-        boolean mergeTimedOut = false;
-        boolean quickTimedOut = false;
-        boolean allTimedOut = false;
+        for (int i = 0; i < 4; i++) {
+            System.out.println();
+            System.out.println("    Using " + genMode[i] + " arrays");
+            System.out.println("           Insertion Sort              Selection Sort                Merge Sort                   Quick Sort        ");
+            System.out.println("    ---Elements----Time-taken--- ---Elements----Time-taken--- ---Elements----Time-taken--- ---Elements----Time-taken---");
+            int size = 256;
+            boolean insertionTimedOut = false;
+            boolean selectionTimedOut = false;
+            boolean mergeTimedOut = false;
+            boolean quickTimedOut = false;
+            boolean allTimedOut = false;
+            String formatString = " | %-10d    %7.4f s  |";
+            String spacerString = " |                          |";
 
-        for (int i = 0; !allTimedOut ; i++) {
-            allTimedOut = insertionTimedOut && selectionTimedOut && mergeTimedOut && quickTimedOut;
-            Integer[] array = ArrayGen.getRandomNumberArray(size);
-            if (!insertionTimedOut) {
-                long startTime = System.nanoTime();
-                Insertion.sort(Arrays.copyOf(array, array.length));
-                long totalTime = System.nanoTime() - startTime;
-                System.out.printf(" * Insertion Sort time for length  %d: %.8f seconds.\n",
-                        size, (totalTime) / BILLION);
-                if (totalTime > 5*BILLION){
-                    insertionTimedOut = true;
-                }
+            while (!allTimedOut && size < 68000000) {
+                allTimedOut = insertionTimedOut && selectionTimedOut && mergeTimedOut && quickTimedOut;
+                Integer[] array = callGenMode(i,size);
+                if (!insertionTimedOut) {
+                    long startTime = System.nanoTime();
+                    Insertion.sort(Arrays.copyOf(array, array.length));
+                    long totalTime = System.nanoTime() - startTime;
+                    System.out.printf("   " + formatString,
+                            size, (totalTime) / BILLION);
+                    if (totalTime > 5 * BILLION) {
+                        insertionTimedOut = true;
+                    }
 
-            } else {
-                //TODO print spacer
-            }
-            if (!selectionTimedOut) {
-                long startTime = System.nanoTime();
-                Selection.sort(Arrays.copyOf(array, array.length));
-                long totalTime = System.nanoTime() - startTime;
-                System.out.printf(" * Selection Sort time for length  %d: %.8f seconds.\n",
-                        size, (totalTime) / BILLION);
-                if (totalTime > 5*BILLION){
-                    selectionTimedOut = true;
+                } else {
+                    System.out.print("   " + spacerString);
                 }
-            } else {
-                //TODO print spacer
-            }
-            if (!mergeTimedOut) {
-                long startTime = System.nanoTime();
-                Merge.sort(Arrays.copyOf(array, array.length));
-                long totalTime = System.nanoTime() - startTime;
-                System.out.printf(" * Merge Sort time for length  %d: %.8f seconds.\n",
-                        size, (totalTime) / BILLION);
-                if (totalTime > 5*BILLION){
-                    mergeTimedOut = true;
+                if (!selectionTimedOut) {
+                    long startTime = System.nanoTime();
+                    Selection.sort(Arrays.copyOf(array, array.length));
+                    long totalTime = System.nanoTime() - startTime;
+                    System.out.printf(formatString,
+                            size, (totalTime) / BILLION);
+                    if (totalTime > 5 * BILLION) {
+                        selectionTimedOut = true;
+                    }
+                } else {
+                    System.out.print(spacerString);
                 }
-            } else {
-                //TODO print spacer
-            }
-            if (!quickTimedOut) {
-                long startTime = System.nanoTime();
-                Quick.sort(Arrays.copyOf(array, array.length));
-                long totalTime = System.nanoTime() - startTime;
-                System.out.printf(" * Quick Sort time for length  %d: %.8f seconds.\n",
-                        size, (totalTime) / BILLION);
-                if (totalTime > 5*BILLION){
-                    quickTimedOut = true;
+                if (!mergeTimedOut) {
+                    long startTime = System.nanoTime();
+                    Merge.sort(Arrays.copyOf(array, array.length));
+                    long totalTime = System.nanoTime() - startTime;
+                    System.out.printf(formatString,
+                            size, (totalTime) / BILLION);
+                    if (totalTime > 5 * BILLION) {
+                        mergeTimedOut = true;
+                    }
+                } else {
+                    System.out.print(spacerString);
                 }
-            } else {
-                //TODO print spacer
+                if (!quickTimedOut) {
+                    long startTime = System.nanoTime();
+                    Quick.sort(Arrays.copyOf(array, array.length));
+                    long totalTime = System.nanoTime() - startTime;
+                    System.out.printf(formatString,
+                            size, (totalTime) / BILLION);
+                    if (totalTime > 5 * BILLION) {
+                        quickTimedOut = true;
+                    }
+                } else {
+                    System.out.print(spacerString);
+                }
+                System.out.println();
+                size *= 2;
             }
-            size *= 2;
+            System.out.println("    ---------------------------- ---------------------------- ---------------------------- ----------------------------");
         }
         //Testing length vs. time (8)
 
@@ -97,72 +110,90 @@ public class A04 {
 //        System.out.print(isUnstable + "\n\n");
     }
 
+    private static Integer[] callGenMode(int genMode, int size){
+        switch (genMode) {
+            case 0 -> {return ArrayGen.getRandomNumberArray(size);}
+            case 1 -> {return ArrayGen.getMostlySortedNumberArray(size);}
+            case 2 -> {return ArrayGen.getReverseOrderNumberArray(size);}
+            case 3 -> {return ArrayGen.getSortedNumberArray(size);}
+            default -> {return new Integer[0];}
+        }
+    }
+
     /**
      * Prints results for various tests on the mystery sorts.
      */
-    private static void getMysterySortTestResults() {//todo array from outside method
-        //System.out.println("= = = = TESTING " + i.toUpperCase() + " SORT = = = =");
-        //System.out.println("\nTIME:"); TODO: need different header
-        int size = 1;
-        boolean sort1TimedOut = false;
-        boolean sort2TimedOut = false;
-        boolean sort3TimedOut = false;
-        boolean sort4TimedOut = false;
-        boolean allTimedOut = false;
+    private static void getMysterySortTestResults() {//todo array from outside method?
+        for (int i = 0; i < 4; i++) {
+            System.out.println();
+            System.out.println("    Using " + genMode[i] + " arrays");
+            System.out.println("                Sort 1                       Sort 2                       Sort 3                       Sort 4       ");
+            System.out.println("    ---Elements----Time-taken--- ---Elements----Time-taken--- ---Elements----Time-taken--- ---Elements----Time-taken---");
+            int size = 256;
+            boolean sort1TimedOut = false;
+            boolean sort2TimedOut = false;
+            boolean sort3TimedOut = false;
+            boolean sort4TimedOut = false;
+            boolean allTimedOut = false;
+            String formatString =" | %-10d    %7.4f s  |";
+            String spacerString =" |                          |";
 
-        while (!allTimedOut) {
-            allTimedOut = sort1TimedOut && sort2TimedOut && sort3TimedOut && sort4TimedOut;
-            Integer[] array = ArrayGen.getRandomNumberArray(size);
-            if (!sort1TimedOut) {
-                long startTime = System.nanoTime();
-                Mystery.sort1(Arrays.copyOf(array, array.length));
-                long totalTime = System.nanoTime() - startTime;
-                System.out.printf(" * Sort1 Sort time for length  %d: %.8f seconds.\n",
-                        size, (totalTime) / BILLION);
-                if (totalTime > 5*BILLION){
-                    sort1TimedOut = true;
-                }
+            while (!allTimedOut && size < 68000000) {
+                allTimedOut = sort1TimedOut && sort2TimedOut && sort3TimedOut && sort4TimedOut;
+                Integer[] array = callGenMode(i,size);
+                if (!sort1TimedOut) {
+                    long startTime = System.nanoTime();
+                    Mystery.sort1(Arrays.copyOf(array, array.length));
+                    long totalTime = System.nanoTime() - startTime;
+                    System.out.printf("   " + formatString,
+                            size, (totalTime) / BILLION);
+                    if (totalTime > 5*BILLION){
+                        sort1TimedOut = true;
+                    }
 
-            } else {
-                //TODO print spacer
-            }
-            if (!sort2TimedOut) {
-                long startTime = System.nanoTime();
-                Mystery.sort2(Arrays.copyOf(array, array.length));
-                long totalTime = System.nanoTime() - startTime;
-                System.out.printf(" * Sort2 Sort time for length  %d: %.8f seconds.\n",
-                        size, (totalTime) / BILLION);
-                if (totalTime > 5*BILLION){
-                    sort2TimedOut = true;
+                } else {
+                    System.out.print("   "+spacerString);
                 }
-            } else {
-                //TODO print spacer
-            }
-            if (!sort3TimedOut) {
-                long startTime = System.nanoTime();
-                Mystery.sort3(Arrays.copyOf(array, array.length));
-                long totalTime = System.nanoTime() - startTime;
-                System.out.printf(" * Sort3 Sort time for length  %d: %.8f seconds.\n",
-                        size, (totalTime) / BILLION);
-                if (totalTime > 5*BILLION){
-                    sort3TimedOut = true;
+                if (!sort2TimedOut) {
+                    long startTime = System.nanoTime();
+                    Mystery.sort2(Arrays.copyOf(array, array.length));
+                    long totalTime = System.nanoTime() - startTime;
+                    System.out.printf(formatString,
+                            size, (totalTime) / BILLION);
+                    if (totalTime > 5*BILLION){
+                        sort2TimedOut = true;
+                    }
+                } else {
+                    System.out.print(spacerString);
                 }
-            } else {
-                //TODO print spacer
-            }
-            if (!sort4TimedOut) {
-                long startTime = System.nanoTime();
-                Mystery.sort4(Arrays.copyOf(array, array.length));
-                long totalTime = System.nanoTime() - startTime;
-                System.out.printf(" * Sort4 Sort time for length  %d: %.8f seconds.\n",
-                        size, (totalTime) / BILLION);
-                if (totalTime > 5*BILLION){
-                    sort4TimedOut = true;
+                if (!sort3TimedOut) {
+                    long startTime = System.nanoTime();
+                    Mystery.sort3(Arrays.copyOf(array, array.length));
+                    long totalTime = System.nanoTime() - startTime;
+                    System.out.printf(formatString,
+                            size, (totalTime) / BILLION);
+                    if (totalTime > 5*BILLION){
+                        sort3TimedOut = true;
+                    }
+                } else {
+                    System.out.print(spacerString);
                 }
-            } else {
-                //TODO print spacer
+                if (!sort4TimedOut) {
+                    long startTime = System.nanoTime();
+                    Mystery.sort4(Arrays.copyOf(array, array.length));
+                    long totalTime = System.nanoTime() - startTime;
+                    System.out.printf(formatString,
+                            size, (totalTime) / BILLION);
+                    if (totalTime > 5*BILLION){
+                        sort4TimedOut = true;
+                    }
+                } else {
+                    System.out.print(spacerString);
+                }
+                size *= 2;
+                System.out.println();
             }
-            size *= 2;
+            System.out.println("    ---------------------------- ---------------------------- ---------------------------- ----------------------------");
         }
     }
 
